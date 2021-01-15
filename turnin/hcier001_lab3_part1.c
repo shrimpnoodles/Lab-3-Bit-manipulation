5a1da5ecc01437811e95be6e01054c7a6cfea226
Hannah Cierlak <hcier001@wch136-24.cs.ucr.edu>
2021-01-15 14:21:17 -0800

    update

--- turnin/hcier001_lab3_part1.c
+++ turnin/hcier001_lab3_part1.c
@@ -24,17 +24,14 @@ int main(void) {
 	
 	unsigned char counter = 0x00;
 	unsigned char i = 0x00;
-	//unsigned char TEMP = 0x00;
     /* Insert your solution below */
     while (1) {
-	//TEMP = PINA;
 	while(i < 8){
 		counter = GetBit(PINA, i) + counter;
 		i++;
 	}
 	i = 0x00; //reset i
-	//TEMP = PINB;
-	while(i < 8){
+	while(i<8){
 		counter = GetBit(PINB, i) + counter;
 		i++;
 	}
