387f03d4740e40e920ec0449f2e67ee2c0f1b4a9
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-01-15 15:59:47 -0800

    Update hcier001_lab3_part2.c

--- turnin/hcier001_lab3_part2.c
+++ turnin/hcier001_lab3_part2.c
@@ -23,51 +23,51 @@ int main(void) {
 	DDRC = 0xFF; PORTC = 0x00;
 	
 	unsigned char fuel = 0x00;
+	unsigned char temp = 0x00;
     /* Insert your solution below */
     while (1) {
 	fuel = PINA;
 	if(fuel ==0x01 || fuel==0x02){
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 6, 1); //gas light
+		SetBit(temp, 5, 1);
+		SetBit(temp, 6, 1); //gas light
 	}
 	if(fuel ==0x03 || fuel ==0x04){
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 4, 1);
-		SetBit(PORTC, 6, 1);
+		SetBit(temp, 5, 1);
+		SetBit(temp, 4, 1);
+		SetBit(temp, 6, 1);
 	}
 	if(fuel ==0x05 || fuel ==0x06){
-		SetBit(PORTC, 6, 0); //gas light off
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 4, 1);
-		SetBit(PORTC, 3, 1);
+		SetBit(temp, 6, 0); //gas light off
+		SetBit(temp, 5, 1);
+		SetBit(temp, 4, 1);
+		SetBit(temp, 3, 1);
 	}
 	if(fuel ==0x07 || fuel ==0x08 || fuel == 0x09){
-		SetBit(PORTC, 6, 0); //gas light off
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 4, 1);
-		SetBit(PORTC, 3, 1);
-		SetBit(PORTC, 2, 1);
+		SetBit(temp, 6, 0); //gas light off
+		SetBit(temp, 5, 1);
+		SetBit(temp, 4, 1);
+		SetBit(temp, 3, 1);
+		SetBit(temp, 2, 1);
 	}
 	if(fuel ==0x0A || fuel ==0x0B || fuel == 0x0C){
-		SetBit(PORTC, 6, 0); //gas light off
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 4, 1);
-		SetBit(PORTC, 3, 1);
-		SetBit(PORTC, 2, 1);
-		SetBit(PORTC, 1, 1);
+		SetBit(temp, 6, 0); //gas light off
+		SetBit(temp, 5, 1);
+		SetBit(temp, 4, 1);
+		SetBit(temp, 3, 1);
+		SetBit(temp, 2, 1);
+		SetBit(temp, 1, 1);
 	}
 	if(fuel ==0x0D || fuel ==0x0E || fuel == 0x0F){
-		SetBit(PORTC, 6, 0); //gas light off
-		SetBit(PORTC, 5, 1);
-		SetBit(PORTC, 4, 1);
-		SetBit(PORTC, 3, 1);
-		SetBit(PORTC, 2, 1);
-		SetBit(PORTC, 1, 1);
-		SetBit(PORTC, 0, 1);
+		SetBit(temp, 6, 0); //gas light off
+		SetBit(temp, 5, 1);
+		SetBit(temp, 4, 1);
+		SetBit(temp, 3, 1);
+		SetBit(temp, 2, 1);
+		SetBit(temp, 1, 1);
+		SetBit(temp, 0, 1);
 	}
-	else{
-		PORTC = 0X00;
-	}
+	PORTC = temp;
+	temp = 0x00;
 	
 }
 
