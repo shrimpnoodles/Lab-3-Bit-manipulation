d17db7ef93d7baabca3cb02f5a9ef65e6b841287
shrimpnoodles <77302524+shrimpnoodles@users.noreply.github.com>
2021-01-15 16:06:24 -0800

    Update hcier001_lab3_part2.c

--- turnin/hcier001_lab3_part2.c
+++ turnin/hcier001_lab3_part2.c
@@ -28,43 +28,43 @@ int main(void) {
     while (1) {
 	fuel = PINA;
 	if(fuel ==0x01 || fuel==0x02){
-		SetBit(temp, 5, 1);
-		SetBit(temp, 6, 1); //gas light
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 6, 1); //gas light
 	}
 	if(fuel ==0x03 || fuel ==0x04){
-		SetBit(temp, 5, 1);
-		SetBit(temp, 4, 1);
-		SetBit(temp, 6, 1);
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 4, 1);
+		temp = SetBit(temp, 6, 1);
 	}
 	if(fuel ==0x05 || fuel ==0x06){
-		SetBit(temp, 6, 0); //gas light off
-		SetBit(temp, 5, 1);
-		SetBit(temp, 4, 1);
-		SetBit(temp, 3, 1);
+		temp = SetBit(temp, 6, 0); //gas light off
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 4, 1);
+		temp = SetBit(temp, 3, 1);
 	}
 	if(fuel ==0x07 || fuel ==0x08 || fuel == 0x09){
-		SetBit(temp, 6, 0); //gas light off
-		SetBit(temp, 5, 1);
-		SetBit(temp, 4, 1);
-		SetBit(temp, 3, 1);
-		SetBit(temp, 2, 1);
+		temp = SetBit(temp, 6, 0); //gas light off
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 4, 1);
+		temp = SetBit(temp, 3, 1);
+		temp = SetBit(temp, 2, 1);
 	}
 	if(fuel ==0x0A || fuel ==0x0B || fuel == 0x0C){
-		SetBit(temp, 6, 0); //gas light off
-		SetBit(temp, 5, 1);
-		SetBit(temp, 4, 1);
-		SetBit(temp, 3, 1);
-		SetBit(temp, 2, 1);
-		SetBit(temp, 1, 1);
+		temp = SetBit(temp, 6, 0); //gas light off
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 4, 1);
+		temp = SetBit(temp, 3, 1);
+		temp = SetBit(temp, 2, 1);
+		temp = SetBit(temp, 1, 1);
 	}
 	if(fuel ==0x0D || fuel ==0x0E || fuel == 0x0F){
-		SetBit(temp, 6, 0); //gas light off
-		SetBit(temp, 5, 1);
-		SetBit(temp, 4, 1);
-		SetBit(temp, 3, 1);
-		SetBit(temp, 2, 1);
-		SetBit(temp, 1, 1);
-		SetBit(temp, 0, 1);
+		temp = SetBit(temp, 6, 0); //gas light off
+		temp = SetBit(temp, 5, 1);
+		temp = SetBit(temp, 4, 1);
+		temp = SetBit(temp, 3, 1);
+		temp = SetBit(temp, 2, 1);
+		temp = SetBit(temp, 1, 1);
+		temp = SetBit(temp, 0, 1);
 	}
 	PORTC = temp;
 	temp = 0x00;
